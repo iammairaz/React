67eefd8c68ac04d239e763cca2f35861fbe904c8
iammairaz <mairazmd457@gmail.com>
2024-07-15 17:57:02 +0530

    tailwind css styling

--- todo-app/frontend/src/components/Login.jsx
+++ todo-app/frontend/src/components/Login.jsx
@@ -18,7 +18,8 @@ export default function Login() {
     navigate(page);
   };
 
-  const login = async () => {
+  const login = async (e) => {
+    e.preventDefault();
     try {
       const response = await axios.post("http://localhost:4000/auth/login", {
         name,
@@ -26,7 +27,7 @@ export default function Login() {
       });
       if (response.status == 200) {
         console.log("login success");
-        const token  = response.data.data;
+        const token = response.data.data;
         setLoginState({
           isLoggedIn: true,
           token,
@@ -49,7 +50,46 @@ export default function Login() {
 
   return (
     <div>
-      <input
+      <form className="max-w-sm mx-auto" onSubmit={login}>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            User Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Please enter user name"
+            required
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              console.log(e.target.value);
+            }}
+          />{" "}
+        </div>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            placeholder="Please enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />{" "}
+        </div>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+      {/*       <input
         type="text"
         placeholder="Please enter user name"
         value={name}
@@ -63,7 +103,7 @@ export default function Login() {
         onChange={(e) => setPassword(e.target.value)}
       />{" "}
       <br />
-      <button onClick={login}>Login</button>
+      <button onClick={login}>Login</button> */}
     </div>
   );
 }
